7097c79c40cb46216125d326dbdd5c8349be6f71
masafm <masa@mkashi.com>
2024-04-28 11:48:28 +0900

    update

--- go_test/greetings/greetings.go
+++ go_test/greetings/greetings.go
@@ -1,8 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+)
 
 // HelloWorld prints "Hello, World!" to the standard output.
 func HelloWorld() {
+    span := tracer.StartSpan("sample", tracer.ResourceName("HelloWorld"))
+    span.SetTag("test", "masafumi")
+    defer span.Finish()
+	
     fmt.Println("Hello, World!")
 }
--- go_test/main.go
+++ go_test/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"go_test/greetings"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
@@ -15,10 +18,30 @@ func main() {
     )
 	defer tracer.Stop()
 
-	span := tracer.StartSpan("web.request", tracer.ResourceName("/posts"))
-    // Set tag
+	span := tracer.StartSpan("sample", tracer.ResourceName("main"))
     span.SetTag("test", "masafumi")
 	defer span.Finish()
 
+	// URLを設定
+	url := "https://www.datadoghq.com/ja/"
+	// http.Clientのインスタンスを作成
+	client := &http.Client{}
+	traceClient := httptrace.WrapClient(client)
+	
+	// HEADリクエストを実行
+	resp, err := traceClient.Head(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close() // 必ずクローズする
+
+	// ヘッダーを取得し、標準出力に出力する
+	for key, values := range resp.Header {
+		for _, value := range values {
+			fmt.Printf("%s: %s\n", key, value)
+		}
+	}
+	
 	greetings.HelloWorld()
 }
